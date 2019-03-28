151987d3ba0787af99a1255b0dff500448b510a2
linfeng <1504902819@qq.com>
2019-03-28 10:20:42 +0800

    linfeng3

--- php_QrReader-master/test.php
+++ php_QrReader-master/test.php
@@ -1,5 +1,8 @@
 <?php
+//引入需要的类
 include_once('./lib/QrReader.php');
+// 将我们要识别的二维码放进去
 $qrcode = new QrReader('./code.png');  //图片路径
-$text = $qrcode->text(); //返回识别后的文本
+//返回识别后的文本
+$text = $qrcode->text(); 
 echo $text;
